68bb2b636870868b5bf071032b30278abb421708
Amit Deshmukh <amit.deshmuk@gmail.com>
2020-03-26 20:22:49 +0800

    Updates

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 
 /**
- * @title SimpleToken
+ * @title TestToken
  * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the sender.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
  */
-contract TestToken is Ownable, Initializable, ERC20, ERC20Detailed {
+contract TestToken is Initializable, ERC20, ERC20Detailed {
     /**
      * @dev initialize that gives msg.sender all of existing tokens.
      */
